0bf6513f3ec06bfc2f8e76a3db59f0284e8f8423
Paul Schreiber <paul@techmatters.org>
2024-09-30 15:47:05 -0400

    fix: update contact form with source field and new text labels

--- wp-content/plugins/bd4d/template-parts/form-email.php
+++ wp-content/plugins/bd4d/template-parts/form-email.php
@@ -10,25 +10,43 @@
 <form id="inline-subscribe" class="newsletter-subscribe" method="post">
 	<div class="form-fields">
 		<div class="signup-input">
-			<label class="screen-reader" for="inline-subscribe-first-name">First Name</label><input id="inline-subscribe-first-name" type="text" name="first_name" placeholder="First name">
+			<label class="screen-reader" for="inline-subscribe-first-name">First Name</label><input id="inline-subscribe-first-name" type="text" name="first_name" placeholder="First name" />
 		</div>
 		<div class="signup-input">
-			<label class="screen-reader" for="inline-subscribe-last-name">Last Name</label><input id="inline-subscribe-last-name" type="text" name="last_name" placeholder="Last name">
+			<label class="screen-reader" for="inline-subscribe-last-name">Last Name</label><input id="inline-subscribe-last-name" type="text" name="last_name" placeholder="Last name" />
 		</div>
 		<div class="signup-input">
-			<label class="screen-reader" for="inline-subscribe-email">Email address</label><input id="inline-subscribe-email" type="email" name="email" placeholder="Email address *" required="required">
+			<label class="screen-reader" for="inline-subscribe-email">Email address</label><input id="inline-subscribe-email" type="email" name="email" placeholder="Email address *" required="required" />
 		</div>
 		<div class="signup-input">
-			<label class="screen-reader" for="inline-subscribe-affiliation">Affiliation</label><input id="inline-subscribe-affiliation" type="text" name="affiliation" autocomplete="organization" placeholder="Affiliation">
+			<label class="screen-reader" for="inline-subscribe-affiliation">Affiliation</label><input id="inline-subscribe-affiliation" type="text" name="affiliation" autocomplete="organization" placeholder="Affiliation" />
+		</div>
+		<div class="signup-input">
+			<label class="screen-reader" for="inline-subscribe-source">How did you hear about A Better Deal For Data?</label>
+			<select id="inline-subscribe-source" name="source" multiple>
+				<option value="Word of Mouth">Word of mouth</option>
+				<option value="Tech Matters">Tech Matters</option>
+				<option value="Online search">Online search</option>
+				<option value="Event">Event</option>
+				<option value="Social media">Social media</option>
+				<option value="Article, blog, or podcast">Article, blog, or podcast</option>
+				<option value="Other">Other</option>
+			</select>
 		</div>
 		<div class="signup-input wide">
-			<label class="screen-reader" for="inline-subscribe-affiliation">Message</label><textarea id="inline-subscribe-message" type="message" rows="5" name="message" placeholder="What is your interest in BD4D? Do you want to contribute a use case or give feedback on our writing?"></textarea>
+			<label class="screen-reader" for="inline-subscribe-affiliation">Message</label><textarea id="inline-subscribe-message" type="message" rows="5" name="message" placeholder="Comments? Feedback? Questions? How do you use data in your community?"></textarea>
 		</div>
 		<div class="signup-input wide">
-			<label for="inline-subscribe-newsletter"><input id="inline-subscribe-newsletter" type="checkbox" name="newsletter" value="true"> Subscribe to the BD4D Newsletter</label>
+			<label for="inline-subscribe-newsletter">
+				<input id="inline-subscribe-newsletter" type="checkbox" name="newsletter" value="true" />
+				Yes! Sign me up to receive email updates
+			</label>
 		</div>
 		<div class="signup-input wide">
-			<label for="inline-subscribe-supporter"><input id="inline-subscribe-supporter" type="checkbox" name="supporter" value="true"> Publicly acknowledge me as a BD4D supporter</label>
+			<label for="inline-subscribe-supporter">
+				<input id="inline-subscribe-supporter" type="checkbox" name="supporter" value="true" />
+				Tell me more about the Coalition of the Willing!
+			</label>
 		</div>
 
 
